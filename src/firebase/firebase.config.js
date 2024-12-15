f22da672114ea5cb903e7e86f99dd27277a5242b
Sultana Razia <sultanarazia483@gmail.com>
2024-12-15 16:04:53 +0600

    Website layout setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCnpj_PrYgsj3MeD_sZIrP8C4GRoN_x4So",
+    authDomain: "tasksphere-d188b.firebaseapp.com",
+    projectId: "tasksphere-d188b",
+    storageBucket: "tasksphere-d188b.firebasestorage.app",
+    messagingSenderId: "780983093796",
+    appId: "1:780983093796:web:c9c1e4b034f2fc66e0a6b1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
