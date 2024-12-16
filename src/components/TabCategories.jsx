954053156c6b736acef7693fa3fd7661f5d4b8eb
Sultana Razia <sultanarazia483@gmail.com>
2024-12-16 13:10:03 +0600

    Job category section added

---
+++ src/components/TabCategories.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import JobCard from './JobCard';
+
+const TabCategories = () => {
+    return (
+        <Tabs>
+            <div className='container px-6 py-10'>
+                <h1 className='text-2xl font-semibold text-center text-gray-800 lg:text-3xl'>Browse Jobs By Categories</h1>
+                <p className='max-w-2xl mx-auto my-6 text-center text-gray-500'>Three categories available for the time being. They are Web Development, Graphics Design and Digital Marketing. Browse them by clicking on the tabs below.</p>
+                <div className='flex justify-center'>
+                    <TabList>
+                        <Tab>Web Development</Tab>
+                        <Tab>Graphics Design</Tab>
+                        <Tab>Digital Marketing</Tab>
+                    </TabList>
+                </div>
+
+                <TabPanel>
+                    <h2>
+                        <JobCard></JobCard>
+                    </h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+            </div>
+        </Tabs>
+    );
+};
+
+export default TabCategories;
