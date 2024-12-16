c4d65ee83325f6eba9de7bd21cad423856f19dfd
Sultana Razia <sultanarazia483@gmail.com>
2024-12-16 10:25:43 +0600

    Slider added in home page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -11,10 +11,15 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Slide from './Slide';
+
+import bgImg1 from '../assets/images/carousel1.jpg';
+import bgImg2 from '../assets/images/carousel2.jpg';
+import bgImg3 from '../assets/images/carousel3.jpg';
 
 export default function Carousel() {
     return (
-        <>
+        <div className='container px-6 py-10 mx-auto'>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -30,16 +35,16 @@ export default function Carousel() {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide>
+                    <Slide image={bgImg1} text='Get Your Web Development Projects Done in Minutes'></Slide>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide image={bgImg2} text='Get Your Graphics design Projects Done in Minutes'></Slide>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide image={bgImg3} text='Start Your Digital Marketing Campaigns up running'></Slide>
+                </SwiperSlide>
             </Swiper>
-        </>
+        </div>
     );
 }
