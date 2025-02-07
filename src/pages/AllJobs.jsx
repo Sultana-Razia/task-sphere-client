d303ca20f5e7c5c60ec7545ee5d5cc64ba54305b
Sultana Razia <sultanarazia483@gmail.com>
2025-02-07 17:21:21 +0600

    Deploy the project to Firebase.

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -10,23 +10,25 @@ const AllJobs = () => {
     const [jobs, setJobs] = useState([]);
     const [filter, setFilter] = useState('');
     const [sort, setSort] = useState('');
+    const [search, setSearch] = useState('');
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_API_URL}/all-jobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}`);
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/all-jobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`);
             setJobs(data);
         }
         getData();
-    }, [currentPage, filter, itemsPerPage, sort]);
+    }, [currentPage, filter, itemsPerPage, sort, search]);
     console.log(jobs);
 
     useEffect(() => {
         const getCount = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_API_URL}/jobs-count?&filter=${filter}`);
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/jobs-count?&filter=${filter}&search=${search}`);
             setCount(data.count);
         }
         getCount();
-    }, [filter]);
+    }, [filter, search]);
     console.log(count);
 
     const numberOfPages = Math.ceil(count / itemsPerPage);
@@ -38,9 +40,18 @@ const AllJobs = () => {
         setCurrentPage(value);
     }
 
+    const handleSearch = e => {
+        e.preventDefault();
+        // const text = e.target.search.value;
+        setSearch(searchText);
+    }
+    console.log(search);
+
     const handleReset = () => {
         setFilter('');
         setSort('');
+        setSearch('');
+        setSearchText('');
     }
 
     return (
@@ -65,12 +76,14 @@ const AllJobs = () => {
                         </select>
                     </div>
 
-                    <form>
+                    <form onSubmit={handleSearch}>
                         <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
                             <input
                                 className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                                 type='text'
                                 name='search'
+                                onChange={e => setSearchText(e.target.value)}
+                                value={searchText}
                                 placeholder='Enter Job Title'
                                 aria-label='Enter Job Title'
                             />
